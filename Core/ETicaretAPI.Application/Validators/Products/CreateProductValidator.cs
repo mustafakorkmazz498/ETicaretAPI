3b455deffd4d9eab6f71913592204394b62bffe7
mustafakorkmazz498 <mustafakorkmaz498498@gmail.com>
2023-08-03 15:21:59 +0300

    Changes

---
+++ Core/ETicaretAPI.Application/Validators/Products/CreateProductValidator.cs
@@ -0,0 +1,39 @@
+﻿using ETicaretAPI.Application.ViewModels.Products;
+using FluentValidation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ETicaretAPI.Application.Validators.Products
+{
+    public class CreateProductValidator : AbstractValidator<VM_Create_Product>
+    {
+        public CreateProductValidator()
+        {
+            RuleFor(p => p.Name)
+                .NotEmpty()
+                .NotNull()
+                    .WithMessage("Lütfen ürün adını boş geçmeyiniz.")
+                .MaximumLength(150)
+                .MinimumLength(2)
+                    .WithMessage("Lütfen adını 2 ile 150 karakter arasında giriniz.");
+
+            RuleFor(p => p.Stock)
+                .NotNull()
+                .NotEmpty()
+                    .WithMessage("Lütfen stok bilgisini boş geçmeyiniz.")
+                .Must(s => s >= 0)
+                    .WithMessage("Stok bilgiisi 0'dan küçük bir değer alamaz.");
+
+            RuleFor(p => p.Price)
+                .NotNull()
+                .NotEmpty()
+                    .WithMessage("Lütfen fiyat bilgisini boş geçmeyiniz.")
+                .Must(s => s >= 0)
+                    .WithMessage("Fiyat bilgiisi 0'dan küçük bir değer alamaz.");
+
+        }
+    }
+}
